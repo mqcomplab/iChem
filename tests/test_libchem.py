0e60a88b234969ec479be917d4a9879f34524ac7
klopezperez <klopezperez@ufl.edu>
2025-10-28 17:33:10 -0400

    initial lichem tests

--- tests/test_libchem.py
+++ tests/test_libchem.py
@@ -1,1 +1,9 @@
-import iChem.libchem
+from iChem.libchem import LibChem
+import numpy as np # type: ignore
+
+MyLib = LibChem()
+fps = np.load('data/ECFP4_fps.npy')
+fps = fps.astype(np.uint8)
+MyLib.load_fingerprints(fps, packed=False)
+print(MyLib.get_iSIM())
+print(MyLib.get_iSIM_sigma(n_sigma_samples=10))
